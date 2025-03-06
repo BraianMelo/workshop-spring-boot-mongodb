5e90428fff1c3ed2948154932c2b5ed259d627ce
BraianMelo <Braian.ptu@gmail.com>
2025-03-06 19:24:53 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/braian/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.braian.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.braian.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
