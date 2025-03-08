1a508af3799f053345a2331655e8b496e7cfcd90
BraianMelo <Braian.ptu@gmail.com>
2025-03-07 21:10:57 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/braian/workshopmongo/resources/UserResource.java
+++ src/main/java/com/braian/workshopmongo/resources/UserResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.braian.workshopmongo.domain.Post;
 import com.braian.workshopmongo.domain.User;
 import com.braian.workshopmongo.dto.UserDTO;
 import com.braian.workshopmongo.services.UserService;
@@ -30,11 +38,19 @@ public class UserResource  {
 	}
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<UserDTO> findAll(@PathVariable String id) {
+	public ResponseEntity<UserDTO> findUserById(@PathVariable String id) {
 		
 		User obj = service.findById(id);
 		
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
