ba71235a839370bb7704549f31e845d8ccf22dac
BraianMelo <Braian.ptu@gmail.com>
2025-03-07 21:19:20 -0300

    Obtendo um post por id

---
+++ src/main/java/com/braian/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.braian.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.braian.workshopmongo.domain.Post;
+import com.braian.workshopmongo.repository.PostRepository;
+import com.braian.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
